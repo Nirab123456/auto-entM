243d0b74fabd87934e1dc1d227c2cba3a13d1772
nirab123456 <rifatulislam210@gmail.com>
2025-10-29 13:03:22 -0400

    Prebuild::continue

--- src/audio_read_send.cpp
+++ src/audio_read_send.cpp
@@ -268,3 +268,130 @@ void AUDIO_RS::I2SReaderLoop()
 }
 
 
+void AUDIO_RS::RingWriterLoop()
+{
+    if (i2s_buffer_.size() == 0 || ring_payload_flat_.size() == 0 || frames_per_packet == 0 || i2s_queue_ == nullptr)
+    {
+        vTaskDelay(pdMS_TO_TICKS(100));
+        vTaskDelete(nullptr);
+        return;
+    }
+    if (ring_payload_flat_.size() % frames_per_packet_ != 0)
+    {
+        Serial.println("RingWriterLoop - ring size not multiple of frames_per_packet");
+        vTaskDelay(pdMS_TO_TICKS(100));
+        vTaskDelete(nullptr);
+        return;     
+    }
+
+    const size_t ring_slots = ring_payload_flat_.size() / frames_per_packet_;
+    const size_t frames = frames_per_packet_;
+    const bool ring_power_of_two = (ring_slots & (ring_slots - 1)) == 0;
+    const size_t ring_mask = ring_power_of_two ? (ring_slots - 1) : 0;
+    
+    for (;;)
+    {
+        size_t bytes_read = 0;
+        if (xQueueReceive(i2s_queue_,&bytes_read,portMAX_DELAY) != pdTRUE)
+        {
+            continue;
+        }
+        size_t word_count = bytes_read / sizeof(uint32_t);
+        size_t available_frames = 0;
+        if (word_count >= (frames * DEFAULT_CHANNEL_COUNT))
+        {
+            available_frames = frames;
+        }
+        else
+        {
+            available_frames = (word_count / DEFAULT_CHANNEL_COUNT);
+        }
+        if ((available_frames > frames))
+        {
+            available_frames = frames;
+        }
+        size_t head = ring_head_sp_ ? ring_head_sp_ -> load(std::memory_order_relaxed) : 0;
+        size_t tail = ring_tail_sp_ ? ring_tail_sp_ -> load(std::memory_order_acquire) : 0;
+        size_t next_head = head + 1;
+
+        if ((next_head -tail) > ring_slots)
+        {
+            if (overrun_policy_ == OverRunPolicy::DROP_NEWEST)
+            {
+                drop_count_newest_.fetch_add(1,std::memory_order_relaxed);
+                if (abs_idx_sp_)
+                {
+                    abs_idx_sp_ -> fetch_add(
+                        (uint64_t)available_frames,
+                        std::memory_order_relaxed
+                    );
+                }
+                taskYIELD();
+                continue;
+            }
+            else
+            {
+                if (ring_tail_sp_)
+                {
+                    ring_tail_sp_ -> fetch_add(
+                        1,
+                        std::memory_order_acq_rel
+                    );
+                    drop_count_oldest_.fetch_add(
+                        1,
+                        std::memory_order_relaxed
+                    );
+                    tail = ring_tail_sp_ ->load(std::memory_order_acquire);
+                }
+                else
+                {
+                    drop_count_newest_.fetch_add(
+                        1,
+                        std::memory_order_relaxed
+                    );
+                    if (abs_idx_sp_)
+                    {
+                        abs_idx_sp_->fetch_add(
+                            (uint64_t)available_frames,
+                            std::memory_order_relaxed
+                        );
+                    }
+                    taskYIELD();
+                    continue;
+                }
+            }
+        }
+        size_t slot = ring_power_of_two ? (head & ring_mask) : (head % ring_slots);
+        uint32_t* row_ptr = ring_payload_flat_.data() + slot * frames;
+        std::span<uint32_t> row(row_ptr, frames);
+        if (available_frames == frames)
+        {
+            for (size_t i = 0; i < frames; i++)
+            {
+                row[i] = i2s_buffer_[i * DEFAULT_CHANNEL_COUNT];
+            }
+            for (size_t i = available_frames; i < frames; i++)
+            {
+                row[i] = 0;
+            }
+        }
+        
+        //meta
+        uint64_t first_sample_idx = abs_idx_sp_ ? abs_idx_sp_->load(std::memory_order_release) : 0;
+        uint64_t ts = (uint64_t)esp_timer_get_time();
+
+        if (ring_head_sp_)
+        {
+            ring_head_sp_->store(next_head,std::memory_order_release);
+        }
+        if (abs_idx_sp_)
+        {
+            abs_idx_sp_->fetch_add(
+                (uint64_t) available_frames,
+                std::memory_order_relaxed
+            );
+        }
+        
+    }
+    
+}
